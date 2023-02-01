b7d8a9b01e34fb214eed5fb567decbff23be3de8
dharmik3193 <dnbambharoliya555@gmail.com>
2023-02-01 16:33:38 +0530

    course

--- src/components/Coursehome.jsx
+++ src/components/Coursehome.jsx
@@ -14,6 +14,7 @@ const Coursehome = () => {
           : 4
   );
   const [course, setCourse] = useState([]);
+  const [isdata, setIsdata] = useState(false);
   // var screen;
   const handleResize = () => {
     if (window.innerWidth <= 1024 && window.innerWidth > 768) {
@@ -41,108 +42,111 @@ const Coursehome = () => {
         // handle success
         console.log(res.data.data);
         setCourse(res.data.data)
+        setIsdata(true)
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       })
   }, [])
-  
 
-  return (
-    <>
-      {/* Courses Section */}
-      <section className="courses-section">
-        <div className="auto-container">
-          <div className="anim-icons">
-            <span className="icon icon-e wow zoomIn" />
-          </div>
-          <div className="sec-title">
-            <span className="sub-title">popular courses</span>
-            <h2>
-              Pick a course to
-              <br />
-              boost up your skills
-            </h2>
-          </div>
-          <div className="carousel-outer">
-            {/* Courses Carousel */}
-            {/* <div className="courses-carousel owl-carousel owl-theme default-nav"> */}
-            <OwlCarousel
-              items={screen}
-              className="owl-theme clients-carousel"
-              loop
-              nav={false}
-              dots={false}
-              margin={8}
-            >
-              {/* Course Block */}
-              {
-                course.map((item,index) => {
-                  return (
-                    <>
-                      <div className="course-block" key={index}>
-                        <div className="inner-box">
-                          <div className="image-box">
-                            <figure className="image">
-                              <a href="page-course-details.html">
-                                <img src={item.image} alt="" />
-                              </a>
-                            </figure>
-                            {/* <span className="price">$49.00</span> */}
-                            <div className="value">{item.level}</div>
-                          </div>
-                          <div className="content-box">
-                            <h5 className="title">
-                              <a href="page-course-details.html">
-                                {item.title}
-                              </a>
-                            </h5>
-                            <div className="other-info">
-                              <div className="rating-box">
-                                <span className="text">({item.rating} /8 Rating)</span>
-                                <div className="rating">
-                                  <span className="fa fa-star" />
-                                  <span className="fa fa-star" />
-                                  <span className="fa fa-star" />
-                                  <span className="fa fa-star" />
-                                  <span className="fa fa-star" />
+  if (isdata) {
+    return (
+      <>
+        {/* Courses Section */}
+        <section className="courses-section">
+          <div className="auto-container">
+            <div className="anim-icons">
+              <span className="icon icon-e wow zoomIn" />
+            </div>
+            <div className="sec-title">
+              <span className="sub-title">popular courses</span>
+              <h2>
+                Pick a course to
+                <br />
+                boost up your skills
+              </h2>
+            </div>
+            <div className="carousel-outer">
+              {/* Courses Carousel */}
+              {/* <div className="courses-carousel owl-carousel owl-theme default-nav"> */}
+              <OwlCarousel
+                items={screen}
+                className="owl-theme clients-carousel"
+                loop
+                nav={false}
+                dots={false}
+                margin={8}
+              >
+                {/* Course Block */}
+                {
+                  course.map((item, index) => {
+                    return (
+                      <>
+                        <div className="course-block" key={index}>
+                          <div className="inner-box">
+                            <div className="image-box">
+                              <figure className="image">
+                                <a href="page-course-details.html">
+                                  <img src={item.image} alt="" />
+                                </a>
+                              </figure>
+                              {/* <span className="price">$49.00</span> */}
+                              <div className="value">{item.level}</div>
+                            </div>
+                            <div className="content-box">
+                              <h5 className="title">
+                                <a href="page-course-details.html">
+                                  {item.title}
+                                </a>
+                              </h5>
+                              <div className="other-info">
+                                <div className="rating-box">
+                                  <span className="text">({item.rating} /8 Rating)</span>
+                                  <div className="rating">
+                                    <span className="fa fa-star" />
+                                    <span className="fa fa-star" />
+                                    <span className="fa fa-star" />
+                                    <span className="fa fa-star" />
+                                    <span className="fa fa-star" />
+                                  </div>
                                 </div>
-                              </div>
-                              <div className="duration">
-                                <i className="fa fa-clock" /> {item.duration}
+                                <div className="duration">
+                                  <i className="fa fa-clock" /> {item.duration}
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </>
-                  )
-                })
-              }
+                      </>
+                    )
+                  })
+                }
 
-            </OwlCarousel>
-            {/* </div> */}
-          </div>
-          <div className="bottom-text">
-            <div className="content">
-              <strong>{course.length}+</strong> more skillful courses you can explore{" "}
-              {/* <a
-                href="page-courses.html"
-                className="theme-btn btn-style-one small"
-              >
-
-              </a> */}
-              <Link to='/courses' className="theme-btn btn-style-one small">
-                Explore All Courses
-              </Link>
+              </OwlCarousel>
+              {/* </div> */}
+            </div>
+            <div className="bottom-text">
+              <div className="content">
+                <strong>{course.length}+</strong> more skillful courses you can explore{" "}
+                {/* <a
+                  href="page-courses.html"
+                  className="theme-btn btn-style-one small"
+                >
+  
+                </a> */}
+                <Link to='/courses' className="theme-btn btn-style-one small">
+                  Explore All Courses
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      {/* End Courses Section*/}
-    </>
-  );
+        </section>
+        {/* End Courses Section*/}
+      </>
+    );
+  }
+
 };
 
 export default Coursehome;
